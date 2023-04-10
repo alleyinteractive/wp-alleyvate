22f1e4e68236c80916eff53e4e15251d1ad7a359
David Herrera <david@alley.com>
2023-04-10 13:41:40 -0400

    Fix tense

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -15,7 +15,7 @@
 		function () {
 			/*
 			 * Turn off all features by default so that we can verify that the behavior
-			 * of WordPress changed after we turn the feature on.
+			 * of WordPress changes after we turn the feature on.
 			 */
 			add_filter( 'alleyvate_load_feature', '__return_false' );
 		},
