01732eef0d5357ab66cab027cb671ada6123d355
Kevin Fodness <2650828+kevinfodness@users.noreply.github.com>
2023-05-18 12:32:38 -0400

    Add failing test for disabling comments

---
+++ tests/alley/wp/alleyvate/features/test-disable-comments.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Class file for Test_Disable_Comments
+ *
+ * (c) Alley <info@alley.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package wp-alleyvate
+ */
+
+namespace Alley\WP\Alleyvate\Features;
+
+use Alley\WP\Alleyvate\Feature;
+use Mantle\Testkit\Test_Case;
+
+/**
+ * Tests for fully disabling comment functionality.
+ */
+final class Test_Disable_Comments extends Test_Case {
+	/**
+	 * Feature instance.
+	 *
+	 * @var Feature
+	 */
+	private Feature $feature;
+
+	/**
+	 * Set up.
+	 */
+	protected function setUp(): void {
+		parent::setUp();
+
+		// $this->feature = new Disable_Comments();
+	}
+
+	/**
+	 * Test that the feature disables comments when active.
+	 */
+	public function test_feature_disable_comments() {
+		$post_id = self::factory()->post->create();
+
+		// Turn off comment flood checking in order to run this test.
+		add_filter( 'wp_is_comment_flood', '__return_false', PHP_INT_MAX );
+
+		// Post a comment on the post and ensure that it posts correctly.
+		$result_pre = wp_handle_comment_submission(
+			[
+				'author'          => 'Test Author',
+				'comment'         => 'Lorem ipsum dolor sit amet.',
+				'comment_parent'  => 0,
+				'comment_post_ID' => $post_id,
+				'email'           => 'user@example.org',
+				'url'             => 'https://example.org',
+			]
+		);
+		$this->assertFalse( is_wp_error( $result_pre ) );
+
+		// TODO: Activate.
+		// $this->feature->boot();
+
+		// Try again, and this time it should fail to insert.
+		$result_post = wp_handle_comment_submission(
+			[
+				'author'          => 'Testy McTesterson',
+				'comment'         => 'A new comment on a post with comments closed.',
+				'comment_parent'  => 0,
+				'comment_post_ID' => $post_id,
+				'email'           => 'testy@example.org',
+				'url'             => 'https://example.org/testy',
+			]
+		);
+		$this->assertTrue( is_wp_error( $result_post ) );
+
+		// Turn on comment flood checking again.
+		remove_filter( 'wp_is_comment_flood', '__return_false', PHP_INT_MAX );
+	}
+}
