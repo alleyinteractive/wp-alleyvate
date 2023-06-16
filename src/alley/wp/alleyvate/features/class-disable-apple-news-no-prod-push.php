3c9dea8f6d22f2eeff9cb998a7924f781aaf6369
Greg Marshall <greg@alley.co>
2023-06-16 17:55:58 -0500

    trying to fix native_function_invocation

--- src/alley/wp/alleyvate/features/class-disable-apple-news-no-prod-push.php
+++ src/alley/wp/alleyvate/features/class-disable-apple-news-no-prod-push.php
@@ -29,7 +29,7 @@ final class Disable_Apple_News_No_Prod_Push implements Feature {
 	 * Boot the feature.
 	 */
 	public function boot(): void {
-		add_filter( 'apple_news_skip_push', [ $this, 'filter_apple_news_skip_push' ], 1, 100 );
+		\add_filter( 'apple_news_skip_push', [ $this, 'filter_apple_news_skip_push' ], 1, 100 );
 		$this->is_production = $this->is_production_environment();
 	}
 
@@ -54,7 +54,7 @@ final class Disable_Apple_News_No_Prod_Push implements Feature {
 		 *
 		 * @param bool $skip Should we skip the Apple News push?
 		 */
-		return apply_filters( 'alleyvate_disable_apple_news_no_prod_push', true );
+		return \apply_filters( 'alleyvate_disable_apple_news_no_prod_push', true );
 	}
 
 	/**
