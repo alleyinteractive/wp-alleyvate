3fcfd155b199701d02648bc6215934429aeaa046
Travis Weston <anubisthejackle@users.noreply.github.com>
2023-11-30 12:02:48 -0500

    Fix PHPCS issues

--- src/alley/wp/alleyvate/features/class-login-nonce.php
+++ src/alley/wp/alleyvate/features/class-login-nonce.php
@@ -44,45 +44,54 @@ final class Login_Nonce implements Feature {
 	 * Boot the feature.
 	 */
 	public function boot(): void {
-		add_action( 'login_init', [$this, 'initialize_nonce_fields'] );
-		add_action( 'login_head', [$this, 'add_meta_refresh'] );
-		add_action( 'login_form', [$this, 'add_nonce_to_form'] );
-		add_filter( 'authenticate', [$this, 'validate_login_nonce'], 9999, 3 );
+		add_action( 'login_init', [ $this, 'initialize_nonce_fields' ] );
+		add_action( 'login_head', [ $this, 'add_meta_refresh' ] );
+		add_action( 'login_form', [ $this, 'add_nonce_to_form' ] );
+		add_filter( 'authenticate', [ $this, 'validate_login_nonce' ], 9999, 3 );
 	}
 
+	/**
+	 * Add a meta refresh to the login page, so it will refresh after the nonce timeout.
+	 */
 	public function add_meta_refresh(): void {
 		if ( ! ( (bool) apply_filters( 'alleyvate_nonce_meta_refresh', true ) ) ) {
 			return;
 		}
 
-		echo sprintf( '<meta http-equiv="refresh" content="%d">', $this->nonce_timeout );
+		echo sprintf( '<meta http-equiv="refresh" content="%d">', esc_attr( $this->nonce_timeout ) );
 	}
 
+	/**
+	 * Add the nonce field to the form.
+	 */
 	public function add_nonce_to_form(): void {
 		wp_nonce_field( $this->nonce_action, $this->nonce_name );
 	}
 
+	/**
+	 * Initializes the nonce fields. Is only run on `login_init` to restrict nonce data to login page.
+	 */
 	public function initialize_nonce_fields(): void {
 		/**
 		 * Filters the nonce name.
 		 *
 		 * @param string $nonce_name The nonce name.
 		 */
-		$this->nonce_name    = (string) apply_filters( 'alleyvate_nonce_name', $this->generate_random_nonce_name('alleyvate_login_nonce') );
+		$this->nonce_name = (string) apply_filters( 'alleyvate_nonce_name', $this->generate_random_nonce_name( 'alleyvate_login_nonce' ) );
 
 		/**
 		 * Filters the nonce action name.
 		 *
 		 * @param string $nonce_action The action name.
 		 */
-		$this->nonce_action  = (string) apply_filters( 'alleyvate_nonce_action', 'alleyvate_login_action' );
+		$this->nonce_action = (string) apply_filters( 'alleyvate_nonce_action', 'alleyvate_login_action' );
 
 		/**
 		 * Filters the lifetime of the nonce, in minutes.
 		 *
 		 * @param int $timeout The lifetime of the nonce, in minutes. Default 30.
 		 */
-		$this->nonce_timeout = ((int) apply_filters( 'alleyvate_nonce_timeout', 30 )) * 60;
+		$this->nonce_timeout = ( (int) apply_filters( 'alleyvate_nonce_timeout', 30 ) ) * 60;
 
 		add_filter( 'nonce_life', fn() => $this->nonce_timeout );
 	}
@@ -91,11 +100,13 @@ final class Login_Nonce implements Feature {
 	 * Validates the passed nonce is valid. Returns a user object on valid login,
 	 * or void on invalid nonce.
 	 *
-	 * @param \WP_User|\WP_Error|null $user The result of previous login validations. An instance of
-	 *                    WP_User if all validations have passed previously.
+	 * @param \WP_User|\WP_Error|null $user     The result of previous login validations. An instance of
+	 *                                          WP_User if all validations have passed previously.
+	 * @param string                  $username The username used to try to login.
+	 * @param string                  $password The password used to try to login.
+	 * @return \WP_User|\WP_Error
 	 */
-	public function validate_login_nonce($user, $username, $password) {
-
+	public function validate_login_nonce( $user, $username, $password ) {
 		/*
 		 * If the filter is returning a \WP_Error, then validation has already failed.
 		 * No need to check the nonce.
@@ -114,8 +125,8 @@ final class Login_Nonce implements Feature {
 		 */
 		if ( null === $user ) {
 			// Remove this check to avoid infinite loops.
-			remove_filter( 'authenticate', [$this, 'validate_login_nonce'], 9999);
-			$user = apply_filters( 'authenticate', $user, $username, $password );
+			remove_filter( 'authenticate', [ $this, 'validate_login_nonce' ], 9999 );
+			$user = apply_filters( 'authenticate', $user, $username, $password ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
 		}
 
 		// We're sure about this value now. Exit early.
@@ -132,19 +143,33 @@ final class Login_Nonce implements Feature {
 
 		// If no post data exists, no nonce data exists, which means our login is invalid.
 		if ( empty( $_POST ) ) {
-			return;
+			return new \WP_Error(
+				'nonce_failure',
+				__( 'Login attempt timed out. Please try again.', 'alley' )
+			);
 		}
 
-		$nonce = $_POST[ $this->nonce_name ] ?? false;
+		$nonce = false;
+
+		if ( ! empty( $_POST[ $this->nonce_name ] ) ) {
+			$nonce = sanitize_key( $_POST[ $this->nonce_name ] );
+		}
 
 		if ( ! $nonce ) {
-			return null;
+			return new \WP_Error(
+				'nonce_failure',
+				__( 'Login attempt timed out. Please try again.', 'alley' )
+			);
+
 		}
 
-		$nonce_validation = wp_verify_nonce($nonce, $this->nonce_action);
+		$nonce_validation = wp_verify_nonce( $nonce, $this->nonce_action );
 
 		if ( ! $nonce_validation ) {
-			return null;
+			return new \WP_Error(
+				'nonce_failure',
+				__( 'Login attempt timed out. Please try again.', 'alley' )
+			);
 		}
 
 		return $user;
@@ -157,13 +182,13 @@ final class Login_Nonce implements Feature {
 	 * @return string
 	 */
 	private function generate_random_nonce_name( string $name ): string {
-		$parts = [$name];
-		if ( ! empty( $_SERVER ) ) {
-			foreach (['REMOTE_ADDR', 'HTTP_X_FORWARDED_FOR', 'HTTP_CLIENT_IP'] as $key ) {
-				$value = ! empty( $_SERVER[ $key ] ) ? $_SERVER[ $key ] : '';
+		$parts = [ $name ];
+		if ( ! empty( $_SERVER ) ) { // phpcs:ignore WordPressVIPMinimum.Variables.ServerVariables.UserControlledHeaders
+			foreach ( [ 'REMOTE_ADDR', 'HTTP_X_FORWARDED_FOR', 'HTTP_CLIENT_IP' ] as $key ) {
+				$value   = ! empty( $_SERVER[ $key ] ) ? sanitize_key( $_SERVER[ $key ] ) : '';
 				$parts[] = "{$key}={$value}";
 			}
 		}
-		return \hash('sha256', implode('-', $parts));
+		return \hash( 'sha256', implode( '-', $parts ) );
 	}
 }
