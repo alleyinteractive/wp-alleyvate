3ac0a3a48e49e2b5ef02f9b2db1b3c132699dd06
Matthew Boynes <git@mattboynes.com>
2025-02-06 18:37:22 -0500

    Sacrifices at the altar of phpstan

--- src/alley/wp/alleyvate/features/class-twitter-embeds.php
+++ src/alley/wp/alleyvate/features/class-twitter-embeds.php
@@ -33,42 +33,42 @@ final class Twitter_Embeds implements Feature {
 	public function boot(): void {
 		add_filter( 'oembed_providers', [ $this, 'add_twitter_oembed_provider' ] );
 		add_filter( 'http_response', [ $this, 'filter_twitter_oembed_404s' ], 10, 3 );
-		add_filter( 'alleyvate_twitter_embeds_404_backstop', [ $this, 'attempt_404_backstop' ], 10, 4 );
+		add_filter( 'alleyvate_twitter_embeds_404_backstop', [ $this, 'attempt_404_backstop' ], 10, 3 );
 	}
 
 	/**
 	 * Add Twitter oEmbed provider.
 	 *
-	 * @param array $providers Array of oEmbed providers.
-	 * @return array
+	 * @param array{string, boolean}[] $providers Array of oEmbed providers.
+	 * @return array{string, boolean}[]
 	 */
 	public function add_twitter_oembed_provider( array $providers ): array {
-		if ( \is_array( $providers ) ) {
-			return array_merge(
-				$providers,
-				[
-					'#https?://(www\.)?x\.com/\w{1,15}/status(es)?/.*#i' => [ 'https://publish.twitter.com/oembed', true ],
-					'#https?://(www\.)?x\.com/\w{1,15}$#i' => [ 'https://publish.twitter.com/oembed', true ],
-					'#https?://(www\.)?x\.com/\w{1,15}/likes$#i' => [ 'https://publish.twitter.com/oembed', true ],
-					'#https?://(www\.)?x\.com/\w{1,15}/lists/.*#i' => [ 'https://publish.twitter.com/oembed', true ],
-					'#https?://(www\.)?x\.com/\w{1,15}/timelines/.*#i' => [ 'https://publish.twitter.com/oembed', true ],
-					'#https?://(www\.)?x\.com/i/moments/.*#i' => [ 'https://publish.twitter.com/oembed', true ],
-				],
-			);
-		}
-		return $providers;
+		return array_merge(
+			$providers,
+			[
+				'#https?://(www\.)?x\.com/\w{1,15}/status(es)?/.*#i' => [ 'https://publish.twitter.com/oembed', true ],
+				'#https?://(www\.)?x\.com/\w{1,15}$#i' => [ 'https://publish.twitter.com/oembed', true ],
+				'#https?://(www\.)?x\.com/\w{1,15}/likes$#i' => [ 'https://publish.twitter.com/oembed', true ],
+				'#https?://(www\.)?x\.com/\w{1,15}/lists/.*#i' => [ 'https://publish.twitter.com/oembed', true ],
+				'#https?://(www\.)?x\.com/\w{1,15}/timelines/.*#i' => [ 'https://publish.twitter.com/oembed', true ],
+				'#https?://(www\.)?x\.com/i/moments/.*#i' => [ 'https://publish.twitter.com/oembed', true ],
+			],
+		);
 	}
 
 	/**
 	 * Attempt to catch 404 responses from Twitter.
 	 *
-	 * @param array  $response    HTTP response.
-	 * @param array  $parsed_args HTTP request arguments.
-	 * @param string $url         URL of the HTTP request.
-	 * @return array
+	 * @param array<mixed> $response    HTTP response.
+	 * @param array<mixed> $parsed_args HTTP request arguments.
+	 * @param string       $url         URL of the HTTP request.
+	 * @return array<mixed>
 	 */
 	public function filter_twitter_oembed_404s( array $response, array $parsed_args, string $url ): array {
-		if ( strpos( $url, 'publish.twitter.com' ) !== false && 404 === $response['response']['code'] ) {
+		if (
+			strpos( $url, 'publish.twitter.com' ) !== false
+		    && 404 === $response['response']['code'] /* @phpstan-ignore offsetAccess.nonOffsetAccessible */
+		) {
 			$this->attempts[ $url ] = ( $this->attempts[ $url ] ?? 0 ) + 1;
 
 			/**
@@ -77,7 +77,7 @@ final class Twitter_Embeds implements Feature {
 			 * @param array  $response    HTTP response.
 			 * @param string $url         URL of the HTTP request.
 			 * @param int    $attempts    Number of times this filter has fired for this URL during this request.
-			 * @param string $parsed_args HTTP request arguments.
+			 * @param array  $parsed_args HTTP request arguments.
 			 */
 			return apply_filters(
 				'alleyvate_twitter_embeds_404_backstop',
@@ -94,10 +94,10 @@ final class Twitter_Embeds implements Feature {
 	/**
 	 * Attempt to catch 404 responses from Twitter.
 	 *
-	 * @param array  $response HTTP response.
-	 * @param string $url      URL of the HTTP request.
-	 * @param int    $attempts Number of times this filter has fired for this URL during this request.
-	 * @return array
+	 * @param array<mixed> $response HTTP response.
+	 * @param string       $url      URL of the HTTP request.
+	 * @param int          $attempts Number of times this filter has fired for this URL during this request.
+	 * @return array<mixed>
 	 */
 	public function attempt_404_backstop( array $response, string $url, int $attempts ): array|WP_Error {
 		if ( 1 === $attempts ) {
@@ -121,11 +121,12 @@ final class Twitter_Embeds implements Feature {
 	/**
 	 * Attempt to use fsockopen transport for Twitter oEmbeds.
 	 *
-	 * @param string $url     URL of the HTTP request.
-	 * @param array  $headers HTTP request headers. Ignored.
-	 * @param array  $data    HTTP request data. Ignored.
-	 * @param string $type    HTTP request type. Ignored.
-	 * @param array  $options HTTP request options. Passed by reference.
+	 * @param string        $url     URL of the HTTP request.
+	 * @param array<string> $headers HTTP request headers. Ignored.
+	 * @param array<mixed>  $data    HTTP request data. Ignored.
+	 * @param string        $type    HTTP request type. Ignored.
+	 * @param array<mixed>  $options HTTP request options. Passed by reference.
+	 * @return void
 	 */
 	public function attempt_fsockopen_for_twitter_oembeds( &$url, &$headers, &$data, &$type, &$options ) {
 		if ( class_exists( Fsockopen::class ) && str_starts_with( $url, 'https://publish.twitter.com/oembed' ) ) {
